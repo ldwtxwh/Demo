83733c2920a2e72581013cabdbb725fe12434259
issuser <issuser@issuserdeMacBook-Pro-3.local>
2014-10-31 11:10:54 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by issuser on 14-10-31.
+//  Copyright (c) 2014年 issuser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Demo
+//
+//  Created by issuser on 14-10-31.
+//  Copyright (c) 2014年 issuser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
